749fd748d246099c2b0e754e539351eeab03a548
startourvip <startourvip886@gmail.com>
2023-04-15 00:17:53 +0800

    1

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require('@nomiclabs/hardhat-ethers');
 require('@openzeppelin/hardhat-upgrades');
 require("@nomicfoundation/hardhat-toolbox");
-const key1='84562d599bb83857f271698d30ca170fef7f878fd1e3503cd2017221114b2ad1'
+const key1='xxx'
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
